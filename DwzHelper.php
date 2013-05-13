f81948bf2d52d065860f46c8e4b5b2ceb7e1c03c
yiqing-95 <yiqing95@yahoo.com.cn>
2013-05-13 12:19:23 +0800

    修改帮助类  现在 DwzHelper::success DwzHelper::error 第二个参数可以接受数组 来传递额外的参数了

--- DwzHelper.php
+++ DwzHelper.php
@@ -8,7 +8,15 @@ class DwzHelper
     /**
      * 这个是给dwz界面用的用于返回相应的消息代码
      * @param $message
-     * @param string $forwardUrl
+     * @param string|array $rel
+     * 数组或者rel参数（参考dwz返回结构中的rel）
+     * 当时数组时可以添加其余dwz支持的变量 因为第一个变量message已经指定
+     * 如果数组中仍然存在一个message键 那么会覆盖第一个的！
+     * array(
+     *   'forwardUrl'=>$this->createUrl(),
+     *   'xx'=>xx
+     * )
+     *
      */
     public static  function success($message, $rel = '')
     {
@@ -19,12 +27,14 @@ class DwzHelper
         $forwardUrl = '';
         $appEnd = true;
 
-        if(!empty($rel)){
-            $navTabId = $rel ;
+        if(is_array($rel)){
+            extract($rel);
         }
 
         $dwzResponse = self::ajaxFormResponse($statusCode, $message, $navTabId, $rel, $callbackType, $forwardUrl);
+
         echo $dwzResponse;
+
         if ($appEnd) {
             Yii::app()->end();
         }
@@ -34,7 +44,10 @@ class DwzHelper
     /**
      *  这个是给dwz界面用的用于返回相应的消息代码
      * @param $message
-     * @param string $forwardUrl
+     * @param string|array $forwardUrl
+     * 同success方法 第二个参数是数组时可以传递额外dwz支持的标准方法
+     * 可用key 参考：
+     * ajaxFormResponse 方法的参数！
      */
     public static   function error($message, $forwardUrl = '')
     {
@@ -49,6 +62,10 @@ class DwzHelper
         $rel = '';
         $callbackType = 'closeCurrent';
         $appEnd = true;
+
+        if(is_array($forwardUrl)){
+            extract($forwardUrl);
+        }
 
         $dwzResponse = self::ajaxFormResponse($statusCode, $message, $navTabId, $rel, $callbackType, $forwardUrl);
         echo $dwzResponse;
